45e198ef86d600fce7614c0071d39e652d473512
Martin Wilches <martindotdev@gmail.com>
2024-06-27 09:31:57 -0500

    Uso de node:test para la realizacion de pruebas unitarias en las funciones dummy y totalLikes

---
+++ utils/list_helper.js
@@ -0,0 +1,7 @@
+// recibe un array de publicaciones de blog como parametro y siempre devuelve el valor de 1
+const dummy = (blogs) => 1
+
+// recibe un array de publicaciones y retorna la suma total de likes de las publicaciones contenidas
+const totalLikes = (blogs) => blogs.reduce((a,b) => a + b.likes, 0)
+
+module.exports = { dummy, totalLikes }
